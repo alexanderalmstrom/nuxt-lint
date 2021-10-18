d32713f9ae4038a1438656423d20bc3cb430e368
Alexander Almström <alexander.almstrom@madepeople.se>
2021-10-18 11:00:20 +0200

    Initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .container {
+    color: #000;
+    font-size: 4rem;
+  }
+
+  .title {
+    color: red;
+  }
+</style>
